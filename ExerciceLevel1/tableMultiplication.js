f792497bc0235c8b96898e160d99d2b92daf5f19
florian-barthelemy <florian-barthelemy@hotmail.com>
2023-11-22 13:38:45 +0100

    ajout exercice table de multiplication

---
+++ ExerciceLevel1/tableMultiplication.js
@@ -0,0 +1,12 @@
+/**
+ * affiche la table de multiplication de 1 à 10 d'un nombre 
+ * @param {number} nombre nombre de la table de multiplication à remplir 
+ */
+function tableMultiplication(nombre){
+    for (iteration=1;iteration<=10;iteration++)
+    {
+        console.log(nombre + " X "+iteration+" = "+(nombre*iteration));
+    }
+}
+
+tableMultiplication(5);
