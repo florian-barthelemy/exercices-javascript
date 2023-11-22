1f58fb88069b3c663a118d039681dc42de9da252
florian-barthelemy <florian-barthelemy@hotmail.com>
2023-11-22 13:20:37 +0100

    ajout exercice profit ou perte

---
+++ ExerciceLevel1/profitOuPerte.js
@@ -0,0 +1,18 @@
+
+/**
+ * dit si un objet/produit est vendu à perte ou à profit
+ * @param {number} prixFabrication prix de fabrication de l'objet/du produit
+ * @param {number} prixVente prix de vente de l'objet/du produit
+ */
+function profitOuPerte(prixFabrication, prixVente){
+    if (prixFabrication>prixVente)
+    {
+        console.log("Perte");
+    }
+    else
+    {
+        console.log("Profit");
+    }
+}
+
+profitOuPerte(5,45);
