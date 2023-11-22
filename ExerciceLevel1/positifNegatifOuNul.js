75a3d8aafbf655b38a44348d13d2bfc2ce54dd59
florian-barthelemy <florian-barthelemy@hotmail.com>
2023-11-22 13:33:53 +0100

    ajout exercice negatif, positif ou nul

---
+++ ExerciceLevel1/positifNegatifOuNul.js
@@ -0,0 +1,19 @@
+
+/**
+ * vérifie si un nombre est négatif, positif ou nul
+ * @param {*} nombre nombre à vérifier
+ */
+function positifNegatifOuNul(nombre){
+        if (nombre<0)
+        {
+            console.log("Nombre négatif");
+        }
+        else if (nombre>0)
+        {
+            console.log("Nombre positif");
+        }
+        else
+        {
+            console.log("Nombre nul");
+        }
+}
